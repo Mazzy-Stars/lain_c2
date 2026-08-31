b6e2d697c4715148ef2a9479c1c4dbf6a06e1d09
Mazzy Stars <3812767681@qq.com>
2026-08-31 15:26:32 +0800

    Update js.go

--- web_ui/js.go
+++ web_ui/js.go
@@ -293,68 +293,6 @@ function appendUniquePrimitiveList(target, incoming) {
     return seen;
 }
 
-function buildCombinedAgentData() {
-    const combined = [];
-    const keys = new Set();
-
-    (Array.isArray(User_data) ? User_data : []).forEach(function(item) {
-        const normalizedItem = normalizeAgentRecord(item);
-        if (!normalizedItem) {
-            return;
-        }
-        const key = getAgentUid(normalizedItem);
-        if (!key) {
-            return;
-        }
-        const index = combined.findIndex(function(existing) {
-            return getAgentUid(existing) === key;
-        });
-        if (index >= 0) {
-            combined[index] = Object.assign({}, combined[index], normalizedItem);
-        } else {
-            combined.push(Object.assign({}, normalizedItem));
-        }
-        keys.add(key);
-    });
-
-    return {
-        data: combined,
-        keys: keys
-    };
-}
-
-function syncSnapshotEntries(target, incoming, keyFn) {
-    const list = normalizeIncomingList(incoming);
-    const next = [];
-    const seen = new Set();
-
-    list.forEach(function(item) {
-        if (!item) {
-            return;
-        }
-        const key = keyFn(item);
-        if (!key) {
-            return;
-        }
-        const index = next.findIndex(function(existing) {
-            return keyFn(existing) === key;
-        });
-        if (index >= 0) {
-            next[index] = Object.assign({}, next[index], item);
-        } else {
-            next.push(item);
-        }
-        seen.add(key);
-    });
-
-    target.length = 0;
-    next.forEach(function(item) {
-        target.push(item);
-    });
-
-    return seen;
-}
-
 function removeMissingNodes(container, selector, keepKeys, getKey) {
     if (!container) {
         return;
