354c6a1c1358158ea7673ec7100d617588ddea10
Mazzy Stars <3812767681@qq.com>
2026-09-08 16:59:35 +0800

    Update index_html.go

--- web_ui/index_html.go
+++ web_ui/index_html.go
@@ -5,13 +5,17 @@ import (
 	"net/http"
 )
 
-func Lain(error_str, web_title, web_js, web_css string, sessionSlice []string) http.HandlerFunc {
+func Lain(error_str, web_title, web_js, web_css string, sessionSlice []string, notFoundHeaders map[string]string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
         //必须先登录
         _, ok := CheckUserSession(r, sessionSlice, error_str)
         if !ok {
+            for k, v := range notFoundHeaders {
+                w.Header().Set(k, v)
+            }
+            w.Header().Set("Content-Type", "text/html; charset=utf-8")
             w.WriteHeader(http.StatusNotFound)
-            fmt.Fprint(w, error_str)
+            _, _ = w.Write([]byte(error_str))
             return
         }
 
